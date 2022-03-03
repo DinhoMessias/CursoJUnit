0ebe002c97c69f269c69f7fbcac03ae682896ed3
Wenderson Messias Soares <wenderson.soares@remsoft.com.br>
2022-03-03 17:53:06 -0300

    Refatoracao das classes de teste

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,8 +1,8 @@
 package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -11,38 +11,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
+    private BonusService service;
+
+
+    @BeforeEach
+    void setUp() {
+        service = new BonusService();
+    }
+
     @Test
     void deveLancarExceptionParaSalarioAcimaDeDezMil() {
-        BonusService service = new BonusService();
         assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Dinho", LocalDate.now(), new BigDecimal("15000"))));
     }
 
     @Test
-    void deveRetornarMensagemErroParaFuncionarioQueNaoTemDireitoBonus(){
-        BonusService service = new BonusService();
-
+    void deveRetornarMensagemErroParaFuncionarioQueNaoTemDireitoBonus() {
         try {
             service.calcularBonus(new Funcionario("Dinho", LocalDate.now(), new BigDecimal("15000")));
             fail("Teste falhou");
-        } catch (Exception e){
+        } catch (Exception e) {
             assertEquals("Funcionario não tem direito a bonus", e.getMessage());
         }
     }
 
     @Test
     void deveRetornarBonusDeQuinhentosReaisParaSalarioDeCincoMil() {
-        BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Dinho", LocalDate.now(), new BigDecimal("5000")));
-
         assertEquals(new BigDecimal("500.0"), bonus);
     }
 
     @Test
     void deveRetornarBonusDeMilReais() {
-        BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Dinho", LocalDate.now(), new BigDecimal("10000")));
-
         assertEquals(new BigDecimal("1000.0"), bonus);
     }
-
 }
--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -2,52 +2,40 @@ package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.enums.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteServiceTest {
 
+    private ReajusteService service;
+    private Funcionario func;
+
+    @BeforeEach
+    void setUp() {
+        service = new ReajusteService();
+        func = new Funcionario("Dinho", LocalDate.now(), new BigDecimal("1000"));
+    }
+
     @Test
     void deveCalcularReajusteParaDesempenhoADesejar(){
-        //cenario
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Dinho", LocalDate.now(), new BigDecimal("1000"));
-
-        //acao
         service.calcularReajuste(func, Desempenho.A_DESEJAR);
-
-        //verificacao
         assertEquals(new BigDecimal("1030.00"), func.getSalario());
     }
 
     @Test
     void deveCalcularReajusteParaDesempenhoBom(){
-        //cenario
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Dinho", LocalDate.now(), new BigDecimal("1000"));
-
-        //acao
         service.calcularReajuste(func, Desempenho.BOM);
-
-        //verificacao
         assertEquals(new BigDecimal("1150.00"), func.getSalario());
     }
 
     @Test
     void deveCalcularReajusteParaDesempenhoOtimo(){
-        //cenario
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Dinho", LocalDate.now(), new BigDecimal("1000"));
-
-        //acao
         service.calcularReajuste(func, Desempenho.OTIMO);
-
-        //verificacao
         assertEquals(new BigDecimal("1200.00"), func.getSalario());
     }
 }
